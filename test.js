206be26f2e5f9956dd2843be11b9fd499503f4e5
Абдурохман <109455493+Fulan-ibn-Fulan@users.noreply.github.com>
2022-07-24 22:44:21 +0300

    Initialize TsybezovSergey/master

---
+++ test.js
@@ -0,0 +1,18 @@
+const student = {
+  firstName:"Petr",
+  lastName:"Perviy"
+}
+const group = []
+
+group.unshift(student)
+
+const student2 = {
+  firstName:"Dmitrii",
+  lastName:"Penkin"
+}
+const student3 = {
+  firstName:"Nikolay",
+  lastName:"Zotov"
+}
+group.push(student2, student3)
+
